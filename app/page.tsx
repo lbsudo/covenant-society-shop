7133f01df9013f922dc755868f84659121a596a7
lbsudo <lbsudo100@gmail.com>
2023-10-14 12:10:13 -0700

    product grid added with Printfull API

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,14 @@ import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code"
 import { button as buttonStyles } from "@nextui-org/theme";
 import { title, subtitle } from "@/components/primitives";
+import { GET } from './api/allProducts/route'
+import ProductGrid from "@/components/product/ProductGrid";
 
-export default function Home() {
+export default async function Home() {
+  const productData = await GET();
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-
+      <ProductGrid products={productData} />
     </section>
   );
 }
---
+++ types/Product.ts
@@ -0,0 +1,96 @@
+export interface SyncProduct {
+  id: number;
+  external_id: string;
+  name: string;
+  synced: number;
+  thumbnail_url: string;
+  is_ignored: boolean;
+}
+
+export interface SyncVariant {
+  id: number;
+  external_id: string;
+  sync_product_id: number;
+  name: string;
+  synced: boolean;
+  variant_id: number;
+  main_category_id: number;
+  warehouse_product_variantid: null | string;
+  retail_price: string;
+  sku: string;
+  currency: string;
+  product: {
+    variant_id: number;
+    product_id: number;
+    image: string;
+    name: string;
+  };
+  files: {
+    id: number;
+    type: string;
+    hash: string;
+    url: null,
+    filename: string;
+    mimeType: string;
+    size: number;
+    width: number;
+    height: number;
+    dpi: number;
+    status: string;
+    created: number;
+  }[];
+  options: {
+    id: string;
+    value: string | string[];
+  }[];
+  is_ignored: boolean;
+  size: string; // Assuming "size" is a string
+  color: string; // Assuming "color" is a string
+  availability_status: string;
+}
+
+
+export interface Product {
+  id: number;
+  external_id: number;
+  name: string;
+  variants: number;
+  synced: number;
+  thumbnail: string;
+  is_ignored: boolean;
+  sync_product: SyncProduct;
+  sync_variant: SyncVariant;
+  sync_variants: SyncVariant[];
+  quantity: number
+  variant: string;
+  price: string;
+}
+
+
+export interface Category {
+  id: number;
+  parent_id: number;
+  image_url: string;
+  catalog_position: number;
+  size: string;
+  title: string;
+}
+
+
+export interface StoreInfo {
+  id: number;
+  name: string;
+  type: string;
+}
+
+
+export interface StateProps {
+  shopping: {
+    productData: Product[],
+    userInfo: {},
+    orderData: {
+      order: Product[];
+    }
+  }
+}
+
