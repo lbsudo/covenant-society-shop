4195b882a873707a89a9c9b350b8d7d23392d6d9
lbsudo <lbsudo100@gmail.com>
2023-10-14 18:07:58 -0700

    changes to product page data fetching

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -7,23 +7,57 @@ interface pageProps {
 }
 
 export default async function ProdPage({ params }: pageProps) {
-  const API_KEY = process.env.NEXT_PUBLIC_PRINTFUL_API_KEY;
-  const API_URL = 'https://api.printful.com';
-  const url = `${API_URL}/store/products/${params.id}`;
-  const res = await fetch(url, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${API_KEY}`,
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-    },
-  });
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
-  }
 
-  const data = await res.json();
-  const product: Product = data.result;
+  const fetchProduct = async () => {
+    const API_KEY = process.env.NEXT_PUBLIC_PRINTFUL_API_KEY;
+    const API_URL = 'https://api.printful.com';
+    const url = `${API_URL}/store/products/${params.id}`;
+    const res = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const data = await res.json();
+    const product: Product = data.result;
+    return product;
+  };
+
+  // const getProduct = async () => {
+  //   try {
+  //     const product = await fetchProduct();
+  //     return product;
+  //   } catch (error) {
+  //     console.error(error);
+  //     return null;
+  //   }
+  // };
+
+  // const API_KEY = process.env.NEXT_PUBLIC_PRINTFUL_API_KEY;
+  // const API_URL = 'https://api.printful.com';
+  // const url = `${API_URL}/store/products/${params.id}`;
+  // const res = await fetch(url, {
+  //   method: 'GET',
+  //   headers: {
+  //     Authorization: `Bearer ${API_KEY}`,
+  //     'Content-Type': 'application/json',
+  //     'Access-Control-Allow-Origin': '*',
+  //   },
+  // });
+  // if (!res.ok) {
+  //   throw new Error('Failed to fetch data');
+  // }
+
+  // const data = await res.json();
+  // const product: Product = data.result;
+
+  const product: Product = await fetchProduct();
 
   return (
     <>
