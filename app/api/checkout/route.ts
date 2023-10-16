fa208151e12e2d3aa5b970f15ded61212943b6c9
lbsudo <lbsudo100@gmail.com>
2023-10-16 12:07:14 -0700

    checkout webhook modified

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -53,46 +53,46 @@ export const POST = async (request: NextRequest) => {
     const recipientEmail = session.customer_email;
 
     // Make an order to the Printful API
-    // const printfulResponse = await fetch("https://api.printful.com/orders", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     Authorization: `Bearer ${process.env.PRINTFUL_API_KEY}`,
-    //   },
-    //   body: JSON.stringify({
-    //     external_id: session.id,
-    //     recipient: {
-    //       name: recipientAddress?.name,
-    //       address1: recipientAddress?.address?.line1,
-    //       city: recipientAddress?.address?.city,
-    //       state_name: recipientAddress?.address?.state,
-    //       country_name: recipientAddress?.address?.country,
-    //       zip: recipientAddress?.address?.postal_code,
-    //       phone: recipientAddress?.phone,
-    //       email: recipientEmail,
-    //     },
-    //     items: items.map((item: Product) => ({
-    //       variant_id: item.sync_variant.variant_id,
-    //       quantity: item.quantity,
-    //       product_template_id: item.sync_variant.sync_product_id,
-    //     })),
-    //   }),
-    // });
+    const printfulResponse = await fetch("https://api.printful.com/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.PRINTFUL_API_KEY}`,
+      },
+      body: JSON.stringify({
+        external_id: session.id,
+        recipient: {
+          name: recipientAddress?.name,
+          address1: recipientAddress?.address?.line1,
+          city: recipientAddress?.address?.city,
+          state_name: recipientAddress?.address?.state,
+          country_name: recipientAddress?.address?.country,
+          zip: recipientAddress?.address?.postal_code,
+          phone: recipientAddress?.phone,
+          email: recipientEmail,
+        },
+        items: items.map((item: Product) => ({
+          variant_id: item.sync_variant.variant_id,
+          quantity: item.quantity,
+          product_template_id: item.sync_variant.sync_product_id,
+        })),
+      }),
+    });
 
-    // // Create a Stripe webhook event
-    // await fetch(process.env.STRIPE_WEBHOOK_URL!, {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify({
-    //     type: "order.completed",
-    //     data: {
-    //       orderId: printfulResponse.json(),
-    //       sessionId: session.id,
-    //     },
-    //   }),
-    // });
+    // Create a Stripe webhook event
+    await fetch(process.env.STRIPE_WEBHOOK_URL!, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        type: "order.completed",
+        data: {
+          orderId: printfulResponse.json(),
+          sessionId: session.id,
+        },
+      }),
+    });
 
     return NextResponse.json({
       message: "Connection is Active",
