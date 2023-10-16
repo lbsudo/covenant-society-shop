de5b0a69ee91e71a61ef2cc3491019e96f13811d
lbsudo <lbsudo100@gmail.com>
2023-10-16 01:52:03 -0700

    auth options modified

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,13 +1,8 @@
 import type { NextAuthOptions } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
-// import GoogleProvider from 'next-auth/providers/google'
 
 export const options: NextAuthOptions = {
   providers: [
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_CLIENT_ID!,
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    // }),
     CredentialsProvider({
       name: "Credentials",
       credentials: {
