116208245967a731681f3c0f6b11fc8748858b5d
lbsudo <lbsudo100@gmail.com>
2023-10-19 02:54:54 -0700

    webhook changes

--- app/api/stripeWebhook/route.ts
+++ app/api/stripeWebhook/route.ts
@@ -1,16 +1,23 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { NextRequest } from 'next/server';
-import Stripe from 'stripe';
-import { buffer } from 'stream/consumers';
+import { NextResponse, NextRequest } from "next/server";
+import Stripe from "stripe";
+import { buffer } from 'micro'
+
+
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   }
+// }
 
 export const POST = async (req: any, res: any) => {
+
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2023-08-16',
+    apiVersion: "2023-08-16",
   });
 
   const buf = await buffer(req);
-  const sig = req.headers.get('stripe-signature');
-  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  const sig = req.headers.get('stripe-signature')
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET
 
   let event;
 
@@ -20,12 +27,12 @@ export const POST = async (req: any, res: any) => {
     event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
   } catch (error: any) {
     console.log(`Webhook error: ${error.message}`);
-    return res.status(400).send(`Webhook error: ${error.message}`);
+    return res.status(400).send(`Webhook error: ${error.message}`)
   }
-
 
   console.log('event', event);
 
   res.status(200).send();
+
 };
 
