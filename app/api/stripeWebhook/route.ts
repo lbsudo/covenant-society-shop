0ec5600602e5371da1441cb779ff9beb1e669bd5
lbsudo <lbsudo100@gmail.com>
2023-10-19 03:40:12 -0700

    webhook changes

--- app/api/stripeWebhook/route.ts
+++ app/api/stripeWebhook/route.ts
@@ -1,15 +1,14 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextRequest } from "next/server";
 import Stripe from "stripe";
-import { buffer } from 'micro'
 
 
-export const POST = async (req: any, res: any) => {
+export const POST = async (req: NextRequest, res: any) => {
 
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
     apiVersion: "2023-08-16",
   });
 
-  const buf = await buffer(req);
+  const buf = await req.text();
   const sig = req.headers.get('stripe-signature')
   const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET
 
@@ -25,6 +24,15 @@ export const POST = async (req: any, res: any) => {
   }
 
   console.log('event', event);
+
+  // Handle the event
+  if (event.type === 'payment_intent.succeeded') {
+    // Perform actions when payment_intent.succeeded event is received
+    // For example, update your database or send a confirmation email
+    const paymentIntent = event.data.object;
+
+    console.log(paymentIntent);
+  }
 
   res.status(200).send();
 
