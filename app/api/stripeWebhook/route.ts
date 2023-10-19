48313dda2435f7a952cb56c3e06f6ff5eb9ef0b2
lbsudo <lbsudo100@gmail.com>
2023-10-19 13:04:30 -0700

    webhook changes

--- app/api/stripeWebhook/route.ts
+++ app/api/stripeWebhook/route.ts
@@ -1,40 +1,51 @@
-import { NextRequest } from "next/server";
-import Stripe from "stripe";
 
+import { NextApiRequest, NextApiResponse } from 'next';
+// import { Readable } from 'stream';
+import Stripe from 'stripe';
 
-export const POST = async (req: NextRequest, res: any) => {
-
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
     apiVersion: "2023-08-16",
   });
 
-  const buf = await req.text();
-  const sig = req.headers.get('stripe-signature')
-  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET
+  let event: Stripe.Event;
 
-  let event;
+  if (req.method === 'POST') {
+    const buf = await new Promise<Buffer>((resolve, reject) => {
+      const chunks: Buffer[] = [];
+      req.on('data', (chunk: Buffer) => chunks.push(chunk));
+      req.on('end', () => resolve(Buffer.concat(chunks)));
+      req.on('error', reject);
+    });
 
-  try {
-    if (!sig || !webhookSecret) return;
+    const sig = req.headers['stripe-signature']!;
+    const secret = process.env.STRIPE_WEBHOOK_SECRET!;
 
-    event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
-  } catch (error: any) {
-    console.log(`Webhook error: ${error.message}`);
-    return res.status(400).send(`Webhook error: ${error.message}`)
+    try {
+      event = stripe.webhooks.constructEvent(buf.toString(), sig, secret);
+    } catch (err: any) {
+      console.log(`⚠️  Webhook signature verification failed.`, err.message);
+      return res.status(400).send('Webhook Error: Invalid signature');
+    }
+
+    switch (event.type) {
+      case 'payment_intent.succeeded':
+        const paymentIntent = event.data.object as Stripe.PaymentIntent;
+        console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+        // handlePaymentIntentSucceeded(paymentIntent);
+        break;
+      case 'payment_method.attached':
+        const paymentMethod = event.data.object as Stripe.PaymentMethod;
+        // handlePaymentMethodAttached(paymentMethod);
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}.`);
+    }
+
+    res.status(200).send('OK');
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
   }
-
-  console.log('event', event);
-
-  // Handle the event
-  if (event.type === 'payment_intent.succeeded') {
-    // Perform actions when payment_intent.succeeded event is received
-    // For example, update your database or send a confirmation email
-    const paymentIntent = event.data.object;
-
-    console.log(paymentIntent);
-  }
-
-  res.status(200).send('Webhook received and processed successfully');
-
-};
+}
 
