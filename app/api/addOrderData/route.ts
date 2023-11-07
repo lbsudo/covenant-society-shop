86bc7617f3015d398bf8c4127466ec0a2502dda7
lbsudo <lbsudo100@gmail.com>
2023-11-06 23:01:14 -0800

    stripe checkout changes - meteadata changes

--- app/api/addOrderData/route.ts
+++ app/api/addOrderData/route.ts
@@ -1,6 +1,7 @@
 import createOrder from "@/lib/createOrder"
 
 export async function POST(req: Request) {
+
   const body = await req.json()
   const order = await createOrder(body);
   return new Response(JSON.stringify(order), {
