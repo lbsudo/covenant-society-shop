fe8bb740f6caf4bd1dfb27f63a47d011f9d54b2a
lbsudo <lbsudo100@gmail.com>
2023-11-13 16:18:08 -0800

    Changes to data fetching to remove limit of products fetched

--- lib/getCategory.ts
+++ lib/getCategory.ts
@@ -1,29 +1,39 @@
-// import { Product } from "../../../types/Product";
 import { Product } from "@/types/Product";
+
 
 export default async function getCategory(mainCategoryIds: Number[]) {
   const API_KEY = process.env.NEXT_PUBLIC_PRINTFUL_API_KEY;
   const API_URL = 'https://api.printful.com';
-  const url = `${API_URL}/store/products?limit=100`;
+  const limit = 20;
+  let offset = 0;
+  let allProducts: Product[] = [];
 
-  const res = await fetch(url, {
-    method: "GET",
-    next: { revalidate: 3500 },
-    headers: {
-      Authorization: `Bearer ${API_KEY}`,
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-  });
+  do {
+    const url = `${API_URL}/store/products?limit=${limit}&offset=${offset}`;
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+    const res = await fetch(url, {
+      method: "GET",
+      next: { revalidate: 3500 },
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
 
-  const data = await res.json();
-  const products: Product[] = await data.result;
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
 
-  const productPromises = products.map((product: Product) =>
+    const data = await res.json();
+    const products: Product[] = await data.result;
+
+    allProducts = allProducts.concat(products);
+
+    offset += limit;
+  } while (allProducts.length === limit);
+
+  const productPromises = allProducts.map((product: Product) =>
     fetch(`${API_URL}/store/products/${product.id}`, {
       method: "GET",
       headers: {
@@ -44,3 +54,4 @@ export default async function getCategory(mainCategoryIds: Number[]) {
 
   return filteredProductDetails;
 }
+
--- lib/getProducts.ts
+++ lib/getProducts.ts
@@ -1,29 +1,40 @@
+
 "use server"
 import { Product } from "@/types/Product";
 
 export default async function getProducts() {
   const API_KEY = process.env.NEXT_PUBLIC_PRINTFUL_API_KEY;
   const API_URL = 'https://api.printful.com';
-  const url = `${API_URL}/store/products?limit=100`;
+  const limit = 20;
+  let offset = 0;
+  let allProducts: Product[] = [];
 
-  const res = await fetch(url, {
-    method: "GET",
-    next: { revalidate: 3500 },
-    headers: {
-      Authorization: `Bearer ${API_KEY}`,
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-  });
+  do {
+    const url = `${API_URL}/store/products?limit=${limit}&offset=${offset}`;
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+    const res = await fetch(url, {
+      method: "GET",
+      next: { revalidate: 3500 },
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
 
-  const data = await res.json();
-  const products: Product[] = await data.result;
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
 
-  const productPromises = products.map((product: Product) =>
+    const data = await res.json();
+    const products: Product[] = await data.result;
+
+    allProducts = allProducts.concat(products);
+
+    offset += limit;
+  } while (allProducts.length === limit);
+
+  const productPromises = allProducts.map((product: Product) =>
     fetch(`${API_URL}/store/products/${product.id}`, {
       method: "GET",
       headers: {
@@ -39,3 +50,4 @@ export default async function getProducts() {
 
   return productDetails;
 }
+
