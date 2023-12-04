21fdd6b69c26dadde605d4826205715019b6eb71
lbsudo <lbsudo100@gmail.com>
2023-12-04 07:47:04 -0800

    dynamic product page function unified

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,37 +2,39 @@ import React from 'react'
 import { Product } from '@/types/Product';
 import ProductPage from "@/components/product/ProductPage";
 import getIds from '@/actions/getId';
+import getProduct from '@/actions/getProduct';
 
 interface pageProps {
   params: { id: string }
 }
 
 export default async function Page({ params }: pageProps) {
+  const prodId = params.id
 
+  // const fetchProduct = async () => {
+  //   const API_KEY = process.env.NEXT_PUBLIC_PRINTFUL_API_KEY;
+  //   const API_URL = 'https://api.printful.com';
+  //   const url = `${API_URL}/store/products/${params.id}`;
 
-  const fetchProduct = async () => {
-    const API_KEY = process.env.NEXT_PUBLIC_PRINTFUL_API_KEY;
-    const API_URL = 'https://api.printful.com';
-    const url = `${API_URL}/store/products/${params.id}`;
+  //   const res = await fetch(url, {
+  //     method: 'GET',
+  //     headers: {
+  //       Authorization: `Bearer ${API_KEY}`,
+  //       'Content-Type': 'application/json',
+  //       'Access-Control-Allow-Origin': '*',
+  //     },
+  //   });
+  //   if (!res.ok) {
+  //     throw new Error('Failed to fetch data');
+  //   }
 
-    const res = await fetch(url, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${API_KEY}`,
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-      },
-    });
-    if (!res.ok) {
-      throw new Error('Failed to fetch data');
-    }
+  //   const data = await res.json();
+  //   const product: Product = await data.result;
+  //   return product;
+  // };
+  const product: Product = await getProduct(prodId)
 
-    const data = await res.json();
-    const product: Product = await data.result;
-    return product;
-  };
-
-  const product: Product = await fetchProduct();
+  // const product: Product = await fetchProduct();
 
   return (
     <>
