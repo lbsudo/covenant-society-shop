37de39fe5d562a58e395fc08ae0c6b06b56b911f
lbsudo <lbsudo100@gmail.com>
2023-12-04 22:19:23 -0800

    types added to category filter function

--- utils/categoryFilter.ts
+++ utils/categoryFilter.ts
@@ -1,5 +1,5 @@
 import { Product } from "@/types/Product";
-export default async function categoryFilter(products: any, mainCategoryIds: number[]) {
+export default async function categoryFilter(products: Product[], mainCategoryIds: number[]) {
   const filteredProductDetails: Product[] = products.filter((product: Product) => {
     return product.sync_variants.some((variant) => mainCategoryIds.includes(variant.main_category_id));
   });
